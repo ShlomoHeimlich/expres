fb232db1309d6972aef4d9849943eaaff8fd65df
shlomo <s0556732899@gmail.com>
2025-07-08 12:13:16 +0300

    first commit

---
+++ expres.js
@@ -0,0 +1,22 @@
+// import express from "express"
+// const PORT=4545;
+// const server=express();
+// server.use(express.json())
+
+
+// server.get('/json',(req,res)=>{
+//     res.send('i from json')
+// })
+
+// server.get('/home',(req,res)=>{
+//    res.send('i from home')
+// })
+
+// server.post('/student',(req,res)=>{
+//     console.log(`get is ${JSON.stringify(req.body)}`);
+//     res.send(`got name:  ${req.body.name}`)
+// })
+
+// server.listen(PORT,()=>{
+//    console.log(`expres is ${PORT}`);
+// })
